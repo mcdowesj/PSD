6f62dae06a131a4d1c939885bc1ddafe8f27006c
Scott McDowell <mcdowesj@tcd.ie>
2015-01-20 15:21:35 +0000

    Initial commit

---
+++ src/main/java/MyClass.java
@@ -0,0 +1,81 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by smcdowell on 20/01/2015.
+ */
+public class MyClass {
+
+
+    //A simple method to concatenate two strings
+    public String concat(String one, String two){
+        return one + two;
+    }
+
+    //checks whether an int is prime or not.
+   public boolean isPrime(int n) {
+        //check if n is a multiple of 2
+        if (n%2==0) return false;
+        //if not, then just check the odds
+        for(int i=3;i*i<=n;i+=2) {
+            if(n%i==0)
+                return false;
+        }
+        return true;
+    }
+
+    public boolean isListPrime(int[] nums){
+        for(int i:nums){
+            if(this.isPrime(i) == false){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void lotsOfFors(){
+        List<String> strings = new ArrayList<String>();
+        List<String> words = new ArrayList<String>();
+        for(int i = 0 ;i<10000; i++){
+            String [] letters = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
+            List<String> alpha =new ArrayList();
+            for(String a:letters){
+                alpha.add(a);
+            }
+            Collections.shuffle(alpha);
+            String word = "";
+            for(int j = 0; j<6; j++){
+                word += alpha.get(j);
+            }
+            words.add(word);
+
+        }
+        for(String word: words){
+            System.out.printf("This is the word : %s\n", word);
+        }
+
+    }
+
+    public List<String> listSorter(List<String> input){
+         Collections.sort(input);
+        return input;
+    }
+    public static void main(String ...args){
+
+        MyClass mc = new MyClass();
+        mc.lotsOfFors();
+        List<String> s = new ArrayList<String>();
+        s.add("Hello ");
+        s.add("World ");
+        s.add(", ");
+        s.add("my ");
+        s.add("name ");
+        s.add("is ");
+        s.add("Joe. ");
+        s = mc.listSorter(s);
+
+    }
+
+}
---
+++ src/test/java/MyTestClass.java
@@ -0,0 +1,53 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by smcdowell on 20/01/2015.
+ */
+public class MyTestClass {
+
+    @Test
+    public void testConcat(){
+        MyClass mc = new MyClass();
+        String result = mc.concat("one", "two");
+        assertEquals(result, "onetwo");
+    }
+
+    @Test
+    public void testIsPrime(){
+      MyClass mc = new MyClass();
+        assertEquals(mc.isPrime(23), true);
+    }
+
+
+    @Test
+    public void testIsListPrime(){
+        MyClass mc = new MyClass();
+        int [] nums = {1,3,5};
+        assertEquals(mc.isListPrime(nums), true);
+
+    }
+
+    @Test
+    public void testListSorter(){
+        MyClass mc = new MyClass();
+        List<String> strings = new ArrayList<String>();
+        strings.add("Hello ");
+        strings.add("World ");
+        strings.add(", ");
+        strings.add("my ");
+        strings.add("name ");
+        strings.add("is ");
+        strings.add("Joe. ");
+        List<String> sorted = strings;
+        Collections.sort(sorted);
+        List<String> maybeSorted = mc.listSorter(strings);
+
+        assertEquals(maybeSorted, sorted);
+
+    }
+}
